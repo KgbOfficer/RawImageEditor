1ed15f6f64a52058f91643faa4914f5ae96eab3b
KgbOfficer <charlesbentley@yandex.com>
2026-05-05 17:06:57 -0400

    Delete src directory

--- src/ui/AdjustmentWidgets/ContrastWidget.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTRASTWIDGET_H
-#define CONTRASTWIDGET_H
-#include <QWidget>
-class ContrastWidget : public QWidget {
-    Q_OBJECT
-public:
-    explicit ContrastWidget(QWidget *parent = nullptr);
-};
-#endif
--- src/ui/AdjustmentWidgets/WhiteBalanceWidget.h
+++
@@ -1,9 +0,0 @@
-#ifndef WHITEBALANCEWIDGET_H
-#define WHITEBALANCEWIDGET_H
-#include <QWidget>
-class WhiteBalanceWidget : public QWidget {
-    Q_OBJECT
-public:
-    explicit WhiteBalanceWidget(QWidget *parent = nullptr);
-};
-#endif
